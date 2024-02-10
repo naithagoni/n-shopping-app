abfb166d665acbac4b41253bd9c3bf9ffd58f6d2
Naresh Aithagoni <naithagoni91@gmail.com>
2024-02-10 20:41:53 +0100

    add(): themes configurations

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,6 +1,27 @@
+import { useEffect, useState } from "react";
 import "./Navbar.scss";
 
 const Navbar = () => {
+  const [theme, setTheme] = useState(
+    localStorage.getItem("theme") ? localStorage.getItem("theme") : "valentine"
+  );
+
+  useEffect(() => {
+    localStorage.setItem("theme", theme as string);
+    const currentTheme = localStorage.getItem("theme");
+    document
+      .querySelector("html")
+      ?.setAttribute("data-theme", currentTheme as string);
+  }, [theme]);
+
+  const handleToggle = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.checked) {
+      setTheme("forest");
+    } else {
+      setTheme("valentine");
+    }
+  };
+
   return (
     <div className="navbar bg-base-100">
       <div className="flex-1">
@@ -75,6 +96,107 @@ const Navbar = () => {
           </ul>
         </div>
       </div>
+      <label className="cursor-pointer grid place-items-center">
+        <input
+          type="checkbox"
+          className="toggle theme-controller bg-base-content row-start-1 col-start-1 col-span-2"
+          onChange={handleToggle}
+          checked={theme === "valentine" ? false : true}
+        />
+        <svg
+          className="col-start-1 row-start-1 stroke-base-100 fill-base-100"
+          xmlns="http://www.w3.org/2000/svg"
+          width="14"
+          height="14"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <circle cx="12" cy="12" r="5" />
+          <path d="M12 1v2M12 21v2M4.2 4.2l1.4 1.4M18.4 18.4l1.4 1.4M1 12h2M21 12h2M4.2 19.8l1.4-1.4M18.4 5.6l1.4-1.4" />
+        </svg>
+        <svg
+          className="col-start-2 row-start-1 stroke-base-100 fill-base-100"
+          xmlns="http://www.w3.org/2000/svg"
+          width="14"
+          height="14"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        >
+          <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>
+        </svg>
+      </label>
+      {/* <div className="dropdown mb-72">
+        <div tabIndex={0} role="button" className="btn m-1">
+          Theme
+          <svg
+            width="12px"
+            height="12px"
+            className="h-2 w-2 fill-current opacity-60 inline-block"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 2048 2048"
+          >
+            <path d="M1799 349l242 241-1017 1017L7 590l242-241 775 775 775-775z"></path>
+          </svg>
+        </div>
+        <ul
+          tabIndex={0}
+          className="dropdown-content z-[1] p-2 shadow-2xl bg-base-300 rounded-box w-52"
+        >
+          <li>
+            <input
+              type="radio"
+              name="theme-dropdown"
+              className="theme-controller btn btn-sm btn-block btn-ghost justify-start"
+              aria-label="Default"
+              value="default"
+            />
+          </li>
+          <li>
+            <input
+              type="radio"
+              name="theme-dropdown"
+              className="theme-controller btn btn-sm btn-block btn-ghost justify-start"
+              aria-label="Retro"
+              value="retro"
+            />
+          </li>
+          <li>
+            <input
+              type="radio"
+              name="theme-dropdown"
+              className="theme-controller btn btn-sm btn-block btn-ghost justify-start"
+              aria-label="Cyberpunk"
+              value="cyberpunk"
+            />
+          </li>
+          <li>
+            <input
+              type="radio"
+              name="theme-dropdown"
+              className="theme-controller btn btn-sm btn-block btn-ghost justify-start"
+              aria-label="Valentine"
+              value="valentine"
+            />
+          </li>
+          <li>
+            <input
+              type="radio"
+              name="theme-dropdown"
+              className="theme-controller btn btn-sm btn-block btn-ghost justify-start"
+              aria-label="Aqua"
+              value="aqua"
+            />
+          </li>
+        </ul>
+      </div> */}
     </div>
   );
 };
